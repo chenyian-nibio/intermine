cebe43818166a7bd7d1edef53ca18355eb48a59b
Yian Chen <chenyian@nibio.go.jp>
2013-02-01 11:28:42 +0900

    Add new source, mirbase-genome

--- bio/sources/targetmine/mirbase-genome/main/src/org/intermine/bio/dataconversion/MirbaseGenomeConverter.java
+++ bio/sources/targetmine/mirbase-genome/main/src/org/intermine/bio/dataconversion/MirbaseGenomeConverter.java
@@ -1,47 +1,135 @@
 package org.intermine.bio.dataconversion;
 
-/*
- * Copyright (C) 2002-2011 FlyMine
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  See the LICENSE file for more
- * information or http://www.gnu.org/copyleft/lesser.html.
- *
- */
-
+import java.io.File;
+import java.io.FileReader;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
-
 
 /**
  * 
- * @author
+ * @author chenyian
  */
-public class MirbaseGenomeConverter extends BioFileConverter
-{
-    //
-    private static final String DATASET_TITLE = "Add DataSet.title here";
-    private static final String DATA_SOURCE_NAME = "Add DataSource.name here";
+public class MirbaseGenomeConverter extends BioFileConverter {
+	// private static Logger LOG = Logger.getLogger(MirbaseGenomeConverter.class);
+	//
+	private static final String DATASET_TITLE = "miRBase";
+	private static final String DATA_SOURCE_NAME = "miRBase";
 
-    /**
-     * Constructor
-     * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
-     */
-    public MirbaseGenomeConverter(ItemWriter writer, Model model) {
-        super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
-    }
+	private Map<String, String> chromosomeMap = new HashMap<String, String>();
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
-    public void process(Reader reader) throws Exception {
+	/**
+	 * Constructor
+	 * 
+	 * @param writer
+	 *            the ItemWriter used to handle the resultant items
+	 * @param model
+	 *            the Model
+	 */
+	public MirbaseGenomeConverter(ItemWriter writer, Model model) {
+		super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
+	}
 
-    }
+	/**
+	 * 
+	 * 
+	 * {@inheritDoc}
+	 */
+	public void process(Reader reader) throws Exception {
+		if (taxonIdMap == null) {
+			readTaxonIdMap();
+		}
+
+		Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(reader);
+		while (iterator.hasNext()) {
+			String[] cols = iterator.next();
+			String chr = cols[0];
+			String type = cols[2];
+
+			String start = cols[3];
+			String end = cols[4];
+			String strand = cols[6];
+			Item location = createItem("Location");
+			location.setAttribute("start", start);
+			location.setAttribute("end", end);
+			location.setAttribute("strand", strand);
+			store(location);
+
+			Map<String, String> ids = new HashMap<String, String>();
+			for (String pair : cols[8].split(";")) {
+				String[] kv = pair.split("=");
+				ids.put(kv[0], kv[1]);
+			}
+			String accession = ids.get("accession_number");
+			Item item;
+			if (type.equals("miRNA")) {
+				item = createItem("MiRNA");
+			} else if (type.equals("miRNA_primary_transcript")) {
+				item = createItem("MiRNAPrimaryTranscript");
+			} else {
+				throw new RuntimeException("Unexpect type: " + type);
+			}
+			item.setAttribute("primaryIdentifier", accession);
+			item.setReference("chromosomeLocation", location);
+
+			item.setReference("chromosome",
+					getChromosome(chr, getTaxonIdByFilename(getCurrentFile().getName())));
+
+			store(item);
+		}
+	}
+
+	private String getChromosome(String chr, String taxonId) throws ObjectStoreException {
+		String key = chr + ":" + taxonId;
+		String ret = chromosomeMap.get(key);
+		if (ret == null) {
+			Item item = createItem("Chromosome");
+			String chrId = chr;
+			if (chr.startsWith("chr")) {
+				chrId = chr.substring(3);
+			}
+			item.setAttribute("primaryIdentifier", chrId);
+			item.setReference("organism", getOrganism(taxonId));
+			store(item);
+			ret = item.getIdentifier();
+			chromosomeMap.put(key, ret);
+		}
+		return ret;
+	}
+	
+	private String getTaxonIdByFilename(String fileName) {
+		String taxonId = taxonIdMap.get(fileName.substring(0, fileName.indexOf(".")));
+		if (taxonId != null) {
+			return taxonId;
+		} else {
+			throw new RuntimeException("unexpected organism code: "
+					+ fileName.substring(0, 3) + " from file: " + fileName);
+		}
+	}
+
+	private Map<String, String> taxonIdMap;
+
+	private File taxonIdFile;
+
+	public void setTaxonIdFile(File taxonIdFile) {
+		this.taxonIdFile = taxonIdFile;
+	}
+
+	private void readTaxonIdMap() throws Exception {
+		taxonIdMap = new HashMap<String, String>();
+		FileReader fileReader = new FileReader(taxonIdFile);
+		Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(fileReader);
+		while (iterator.hasNext()) {
+			String[] cols = iterator.next();
+			taxonIdMap.put(cols[1], cols[0]);
+		}
+	}
+
 }
