6ffa1aa6b2351a7b89f40f12167aa989142c0a74
Yian Chen <chenyian@nibio.go.jp>
2014-05-21 10:22:16 +0900

    refine the TF model of oreganno

--- bio/sources/targetmine/oreganno/main/src/org/intermine/bio/dataconversion/OregannoConverter.java
+++ bio/sources/targetmine/oreganno/main/src/org/intermine/bio/dataconversion/OregannoConverter.java
@@ -12,45 +12,45 @@ import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
 
-
 /**
  * 
  * @author chenyian
  */
-public class OregannoConverter extends BioFileConverter
-{
-//	private static final Logger LOG = Logger.getLogger(OregannoConverter.class);
+public class OregannoConverter extends BioFileConverter {
+	// private static final Logger LOG = Logger.getLogger(OregannoConverter.class);
 
 	//
-    private static final String DATASET_TITLE = "ORegAnno";
-    private static final String DATA_SOURCE_NAME = "ORegAnno";
+	private static final String DATASET_TITLE = "ORegAnno";
+	private static final String DATA_SOURCE_NAME = "ORegAnno";
 
 	private static final String INTERACTION_TYPE = "Transcriptional regulation";
 
 	private Map<String, String> geneMap = new HashMap<String, String>();
-	
-	
-    /**
-     * Constructor
-     * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
-     */
-    public OregannoConverter(ItemWriter writer, Model model) {
-        super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
-    }
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
-    public void process(Reader reader) throws Exception {
+	/**
+	 * Constructor
+	 * 
+	 * @param writer
+	 *            the ItemWriter used to handle the resultant items
+	 * @param model
+	 *            the Model
+	 */
+	public OregannoConverter(ItemWriter writer, Model model) {
+		super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
+	}
+
+	/**
+	 * 
+	 * 
+	 * {@inheritDoc}
+	 */
+	public void process(Reader reader) throws Exception {
 		Iterator<String[]> iterator = FormattedTextParser
 				.parseTabDelimitedReader(new BufferedReader(reader));
 
 		while (iterator.hasNext()) {
 			String[] cols = iterator.next();
-			// source target sequence start end chromosome strand genomeBuild stableId 
+			// source target sequence start end chromosome strand genomeBuild stableId
 			String sourceId = cols[0].trim();
 			String targetId = cols[1].trim();
 			if (sourceId == null || sourceId.equals("")) {
@@ -60,20 +60,23 @@ public class OregannoConverter extends BioFileConverter
 
 			if (targetId.equals(sourceId)) {
 				// create Interaction
-				createInteraction(sourceId, sourceId, "source&target", bindingStie);
+				getInteraction(sourceId, sourceId, "source&target").addToCollection("bindingSites",
+						bindingStie);
 
 			} else {
 				// create Interaction for source
-				createInteraction(sourceId, targetId, "source", bindingStie);
+				getInteraction(sourceId, targetId, "source").addToCollection("bindingSites",
+						bindingStie);
 
 				// create Interaction for target
-				createInteraction(targetId, sourceId, "target", bindingStie);
+				getInteraction(targetId, sourceId, "target").addToCollection("bindingSites",
+						bindingStie);
 			}
-			
+
 		}
 		reader.close();
 	}
-    
+
 	private Item getBindingSite(String[] cols) throws ObjectStoreException {
 		// TODO chenyian to be refined; use the default sequence ontology (SO) model
 		Item ret = createItem("BindingSiteInfo");
@@ -98,17 +101,22 @@ public class OregannoConverter extends BioFileConverter
 		return ret;
 	}
 
-	private void createInteraction(String masterId, String slaveId, String role,
-			Item bindingSite) throws ObjectStoreException {
-		Item item = createItem("ProteinDNAInteraction");
-		item.setReference("gene", getGene(masterId));
-		item.setReference("interactWith", getGene(slaveId));
+	Map<String, Item> interactionMap = new HashMap<String, Item>();
 
-		item.setAttribute("interactionType", INTERACTION_TYPE);
-		item.setAttribute("name", String.format("AMADEUS_G%s_G%s", masterId, slaveId));
-		item.setAttribute("role", role);
-		item.setReference("bindingSite", bindingSite);
-		store(item);
+	private Item getInteraction(String masterId, String slaveId, String role)
+			throws ObjectStoreException {
+		String key = slaveId + role + masterId;
+		Item ret = interactionMap.get(key);
+		if (ret == null) {
+			ret = createItem("ProteinDNAInteraction");
+			ret.setReference("gene", getGene(masterId));
+			ret.setReference("interactWith", getGene(slaveId));
+
+			ret.setAttribute("interactionType", INTERACTION_TYPE);
+			ret.setAttribute("role", role);
+			interactionMap.put(key, ret);
+		}
+		return ret;
 	}
 
 	private String getGene(String ncbiGeneId) throws ObjectStoreException {
@@ -122,6 +130,11 @@ public class OregannoConverter extends BioFileConverter
 			geneMap.put(ncbiGeneId, ret);
 		}
 		return ret;
+	}
+
+	@Override
+	public void close() throws Exception {
+		store(interactionMap.values());
 	}
 
 }
