054c59ed0d868d74de4b799c6fcf7cb5e035648d
Yian Chen <chenyian@nibio.go.jp>
2014-05-21 17:14:40 +0900

    edit the header descriptions of the paser

--- bio/sources/targetmine/htridb/main/src/org/intermine/bio/dataconversion/HtridbConverter.java
+++ bio/sources/targetmine/htridb/main/src/org/intermine/bio/dataconversion/HtridbConverter.java
@@ -1,15 +1,5 @@
 package org.intermine.bio.dataconversion;
 
-/*
- * Copyright (C) 2002-2011 FlyMine
- *
- * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  See the LICENSE file for more
- * information or http://www.gnu.org/copyleft/lesser.html.
- *
- */
-
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -23,8 +13,9 @@ import org.intermine.xml.full.Item;
 
 
 /**
+ * Parser for HtriDB (http://www.lbbc.ibb.unesp.br/htri)
  * 
- * @author
+ * @author chenyian
  */
 public class HtridbConverter extends BioFileConverter
 {
