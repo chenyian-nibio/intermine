94430a4b7cbe18343d287de9fb3e45b59ef18796
Yian Chen <chenyian@nibio.go.jp>
2014-05-22 15:54:28 +0900

    add a retriever to fill the basic info of the genes without proper annotations

--- bio/sources/targetmine/fill-missing-proteins/main/src/org/intermine/bio/dataconversion/MissingProteinRetriever.java
+++ bio/sources/targetmine/fill-missing-proteins/main/src/org/intermine/bio/dataconversion/MissingProteinRetriever.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
