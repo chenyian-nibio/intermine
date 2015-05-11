79580c8d66bdbbf65e6574973c4f9bd56b876b71
Yian Chen <chenyian@nibio.go.jp>
2015-05-11 16:53:36 +0900

    associate all proteins with enzyme class regardless of the species

--- bio/sources/targetmine/enzyme/main/src/org/intermine/bio/dataconversion/EnzymeConverter.java
+++ bio/sources/targetmine/enzyme/main/src/org/intermine/bio/dataconversion/EnzymeConverter.java
@@ -5,17 +5,24 @@ import java.io.Reader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
 import org.intermine.metadata.StringUtil;
+import org.intermine.model.bio.Protein;
+import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.objectstore.ObjectStoreFactory;
+import org.intermine.objectstore.query.Query;
+import org.intermine.objectstore.query.QueryClass;
+import org.intermine.objectstore.query.QueryField;
+import org.intermine.objectstore.query.Results;
+import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.xml.full.Item;
 
 /**
@@ -26,13 +33,9 @@ import org.intermine.xml.full.Item;
  * wget ftp://ftp.expasy.org/databases/enzyme/enzyme.dat
  * </pre>
  * 
- * one option should be contained in project.xml setting for organisms.<br/>
- * the values are the codes used in uniprot protein name (primaryIdentifier), <br/>
- * such as HUMAN in 1433B_HUMAN
- * 
- * <pre>
- * &lt;property name=&quot;enzyme.organisms&quot; value=&quot;HUMAN RAT MOUSE DROME&quot;/&gt;
- * </pre>
+ * The parser will try to associate all proteins found in the built database with the enzymes 
+ * regardless of species. <br/> 
+ * Better to use after all proteins are incorporated. 
  * 
  * @author chenyian
  */
@@ -46,8 +49,8 @@ public class EnzymeConverter extends BioFileConverter {
 	private Set<String> organismNames;
 
 	public void setEnzymeOrganisms(String organismNames) {
-		this.organismNames = new HashSet<String>(Arrays
-				.asList(StringUtil.split(organismNames, " ")));
+		this.organismNames = new HashSet<String>(
+				Arrays.asList(StringUtil.split(organismNames, " ")));
 		LOG.info("Setting list of organisms to " + this.organismNames);
 	}
 
@@ -71,6 +74,8 @@ public class EnzymeConverter extends BioFileConverter {
 	 * {@inheritDoc}
 	 */
 	public void process(Reader reader) throws Exception {
+		queryAllProteinAccs();
+
 		BufferedReader br = new BufferedReader(reader);
 		String l;
 		boolean isEntry = false;
@@ -87,7 +92,7 @@ public class EnzymeConverter extends BioFileConverter {
 			}
 
 			if (l.startsWith("DR")) {
-				Set<String> accs = parsePrimaryAcc(l.substring(5));
+				Set<String> accs = processPrimaryAcc(l.substring(5));
 				if (!accs.isEmpty()) {
 					ee.proteins.addAll(accs);
 				}
@@ -106,13 +111,15 @@ public class EnzymeConverter extends BioFileConverter {
 		br.close();
 	}
 
-	private Set<String> parsePrimaryAcc(String proteinString) {
-		Pattern pattern = Pattern.compile("(\\w+)_(\\w+)");
-		Matcher matcher = pattern.matcher(proteinString);
+	private Set<String> processPrimaryAcc(String proteinString) {
 		Set<String> ret = new HashSet<String>();
-		while (matcher.find()) {
-			if (organismNames.contains(matcher.group(2))) {
-				ret.add(matcher.group(0));
+		String[] proteinEntries = proteinString.split("  ");
+		for (String entry : proteinEntries) {
+			if (entry.indexOf(",") > 0) {
+				String acc = entry.substring(0, entry.indexOf(","));
+				if (proteinAccs.contains(acc)) {
+					ret.add(acc);
+				}
 			}
 		}
 		return ret;
@@ -140,8 +147,8 @@ public class EnzymeConverter extends BioFileConverter {
 			enzyme.setAttribute("cofactor", cofactor.replaceAll("\\.$", ""));
 		}
 
-		for (String proteinName : enzymeEntry.proteins) {
-			String refId = getProtein(proteinName);
+		for (String primaryAccession : enzymeEntry.proteins) {
+			String refId = getProtein(primaryAccession);
 			if (refId != null) {
 				enzyme.addToCollection("proteins", refId);
 			}
@@ -155,24 +162,16 @@ public class EnzymeConverter extends BioFileConverter {
 			enzyme.addToCollection("synonyms", item);
 		}
 
-		// not necessary anymore?
-		// add EC number to synonyms, for quick searching
-//		Item item = createItem("Synonym");
-//		item.setReference("subject", enzyme.getIdentifier());
-//		item.setAttribute("value", enzymeEntry.ecNumber);
-//		store(item);
-//		enzyme.addToCollection("synonyms", item);
-
 		store(enzyme);
 	}
 
-	private String getProtein(String proteinName) throws ObjectStoreException {
-		String ret = proteinMap.get(proteinName);
+	private String getProtein(String primaryAccession) throws ObjectStoreException {
+		String ret = proteinMap.get(primaryAccession);
 		if (ret == null) {
 			Item protein = createItem("Protein");
-			protein.setAttribute("primaryIdentifier", proteinName);
+			protein.setAttribute("primaryAccession", primaryAccession);
 			ret = protein.getIdentifier();
-			proteinMap.put(proteinName, ret);
+			proteinMap.put(primaryAccession, ret);
 			store(protein);
 		}
 		return ret;
@@ -186,15 +185,44 @@ public class EnzymeConverter extends BioFileConverter {
 
 		protected String altName = "";
 		protected Set<String> proteins = new HashSet<String>();
-		
+
 		protected Set<String> getSynonyms() {
 			Set<String> synonyms = new HashSet<String>();
-			for (String an: altName.split("\\.")) {
+			for (String an : altName.split("\\.")) {
 				if (!StringUtils.isEmpty(an.trim())) {
 					synonyms.add(an.trim());
 				}
 			}
 			return synonyms;
+		}
+	}
+
+	private Set<String> proteinAccs = new HashSet<String>();
+
+	private String osAlias = null;
+
+	public void setOsAlias(String osAlias) {
+		this.osAlias = osAlias;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void queryAllProteinAccs() throws Exception {
+
+		Query q = new Query();
+		QueryClass qcProtein = new QueryClass(Protein.class);
+
+		QueryField qfPrimaryAcc = new QueryField(qcProtein, "primaryAccession");
+
+		q.addFrom(qcProtein);
+		q.addToSelect(qfPrimaryAcc);
+
+		ObjectStore os = ObjectStoreFactory.getObjectStore(osAlias);
+
+		Results results = os.execute(q);
+		Iterator<Object> iterator = results.iterator();
+		while (iterator.hasNext()) {
+			ResultsRow<String> rr = (ResultsRow<String>) iterator.next();
+			proteinAccs.add(rr.get(0));
 		}
 	}
 
