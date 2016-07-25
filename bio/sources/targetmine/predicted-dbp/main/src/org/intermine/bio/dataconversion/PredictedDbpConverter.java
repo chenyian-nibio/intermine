8af9ef037c45f4f97579b651403b6f5d1aefbbb0
Yian Chen <chenyian@nibiohn.go.jp>
2016-07-25 17:21:22 +0900

    add DNA binding site prediction data

--- bio/sources/targetmine/predicted-dbp/main/src/org/intermine/bio/dataconversion/PredictedDbpConverter.java
+++ bio/sources/targetmine/predicted-dbp/main/src/org/intermine/bio/dataconversion/PredictedDbpConverter.java
@@ -1,5 +1,8 @@
 package org.intermine.bio.dataconversion;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,54 +19,95 @@ import org.intermine.xml.full.Item;
  * 
  * @author chenyian
  */
-public class PredictedDbpConverter extends BioFileConverter
-{
-    private static final String ANNOTATION_TYPE = "DNA binding";
+public class PredictedDbpConverter extends BioFileConverter {
+	private static final String ANNOTATION_TYPE = "DNA binding";
 	//
 
 	private Map<String, String> proteinMap = new HashMap<String, String>();
-	
-	
-    /**
-     * Constructor
-     * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
-     */
-    public PredictedDbpConverter(ItemWriter writer, Model model) {
-        super(writer, model);
-    }
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
-    public void process(Reader reader) throws Exception {
-    	Iterator<String[]> iterator = FormattedTextParser.parseDelimitedReader(reader, ' ');
-    	// skip header ..
-    	iterator.next();
-    	while (iterator.hasNext()) {
-    		String[] cols = iterator.next();
-    		String accession = cols[0];
-    		Float consensus2 = Float.valueOf(cols[4]);
-    		Float precision = Float.valueOf(cols[12]);
-    		
-    		if (consensus2 >= 0.12f) {
-    			String confidence = "medium";
-    			if (consensus2 >= 0.22) {
-    				confidence = "high";
-    			}
-    			Item item = createItem("PredictedAnnotation");
-    			item.setAttribute("type", ANNOTATION_TYPE);
-    			item.setAttribute("confidence", confidence);
-    			item.setAttribute("score", precision.toString());
-    			item.setReference("protein", getProtein(accession));
-    			store(item);
-    		}
-    	}
+	/**
+	 * Constructor
+	 * 
+	 * @param writer the ItemWriter used to handle the resultant items
+	 * @param model the Model
+	 */
+	public PredictedDbpConverter(ItemWriter writer, Model model) {
+		super(writer, model);
+	}
+
+	/**
+	 * 
+	 * 
+	 * {@inheritDoc}
+	 */
+	public void process(Reader reader) throws Exception {
+		// create PredictedAnnotation first
+		processPredictedDBP();
+
+		// read-in the predicted binding site, and associated with PredictedAnnotation if available
+		Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(reader);
+		while (iterator.hasNext()) {
+			String[] cols = iterator.next();
+			String accession = cols[0];
+			String sites = cols[1];
+			
+			for (String site: sites.split(",")) {
+				String position = site.substring(1);
+				Item item = createItem("PredictedRegion");
+				item.setAttribute("start", position);
+				item.setAttribute("end", position);
+				item.setAttribute("type", ANNOTATION_TYPE);
+				item.setReference("protein", getProtein(accession));
+				
+				if (predictedAnnotationMap.get(accession) != null) {
+					item.setReference("prediction", predictedAnnotationMap.get(accession));
+				}
+				
+				store(item);
+			}
+		}
     }
     
-	private String getProtein(String primaryAccession) throws ObjectStoreException {
+	private File dbpScoreFile;
+
+	public void setDbpScoreFile(File dbpScoreFile) {
+		this.dbpScoreFile = dbpScoreFile;
+	}
+
+	private Map<String, String> predictedAnnotationMap = new HashMap<String, String>();
+    
+    private void processPredictedDBP() throws ObjectStoreException, IOException {
+		if (dbpScoreFile == null) {
+			throw new RuntimeException("pathwayClassFile property not set");
+		}
+		
+		Iterator<String[]> iterator = FormattedTextParser.parseDelimitedReader(new FileReader(
+				dbpScoreFile), ' ');
+    	// skip header ..
+		iterator.next();
+		while (iterator.hasNext()) {
+			String[] cols = iterator.next();
+			String accession = cols[0];
+			Float consensus2 = Float.valueOf(cols[4]);
+			Float precision = Float.valueOf(cols[12]);
+
+			if (consensus2 >= 0.12f) {
+				String confidence = "medium";
+				if (consensus2 >= 0.22) {
+					confidence = "high";
+				}
+				Item item = createItem("PredictedAnnotation");
+				item.setAttribute("type", ANNOTATION_TYPE);
+				item.setAttribute("confidence", confidence);
+				item.setAttribute("score", precision.toString());
+				item.setReference("protein", getProtein(accession));
+				store(item);
+				predictedAnnotationMap.put(accession, item.getIdentifier());
+			}
+		}
+    }
+
+    private String getProtein(String primaryAccession) throws ObjectStoreException {
 		String ret = proteinMap.get(primaryAccession);
 		if (ret == null) {
 			Item item = createItem("Protein");
