7bc6ea76766644a1eb24e69e7888dc8c41561d10
Yian Chen <chenyian@nibiohn.go.jp>
2018-04-19 17:55:37 +0900

    improve disgenet parser; add publications and cross reference for the disease terms

--- bio/sources/targetmine/disgenet/main/src/org/intermine/bio/dataconversion/DisgenetConverter.java
+++ bio/sources/targetmine/disgenet/main/src/org/intermine/bio/dataconversion/DisgenetConverter.java
@@ -1,10 +1,17 @@
 package org.intermine.bio.dataconversion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
 import org.intermine.objectstore.ObjectStoreException;
@@ -18,13 +25,28 @@ import org.intermine.xml.full.Item;
  * 
  * @author chenyian
  */
-public class DisgenetConverter extends BioFileConverter
-{
+public class DisgenetConverter extends BioFileConverter {
+	
+	private static final Logger LOG = Logger.getLogger(DisgenetConverter.class);
     //
 	private static final String DATASET_TITLE = "DisGeNET";
 	private static final String DATA_SOURCE_NAME = "DisGeNET";
-	
+
+    private File pmidFile;
+
+    public void setPmidFile(File pmidFile) {
+		this.pmidFile = pmidFile;
+	}
+
+    private File diseaseMapFile;
+    
+    public void setDiseaseMapFile(File diseaseMapFile) {
+    	this.diseaseMapFile = diseaseMapFile;
+    }
+    
 	private Map<String, String> sourceNameMap = new HashMap<String, String>();
+	private Map<String, Set<Integer>> pmidMap = new HashMap<String, Set<Integer>>();
+	private Map<String, Set<String>> ontologyIdMap = new HashMap<String, Set<String>>();
 
     /**
      * Constructor
@@ -48,6 +70,9 @@ public class DisgenetConverter extends BioFileConverter
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
+    	readPmidMap();
+    	readDiseaseMap();
+    	
     	Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(reader);
     	
     	// first line is the header
@@ -69,11 +94,100 @@ public class DisgenetConverter extends BioFileConverter
     				item.addToCollection("sources", getDataSource(name));
     			}
     		}
+			String key = String.format("%s-%s", geneId, diseaseId);
+			if (pmidMap.get(key) != null) {
+				for (Integer pmid : pmidMap.get(key)) {
+					item.addToCollection("publications", getPublication(pmid.toString()));
+				}
+			}
+			
     		store(item);
     	}
 
     }
 
+	private void readPmidMap() {
+		String fn = pmidFile.getName();
+		LOG.info(String.format("Parsing the file %s ......", fn));
+		System.out.println(String.format("Parsing the file %s ......", fn));
+		
+		try {
+			FileReader reader = new FileReader(pmidFile);
+			Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(reader);
+
+			// first line is the header
+	    	iterator.next();
+	    	
+			while (iterator.hasNext()) {
+				String[] cols = iterator.next();
+				String geneId = cols[0];
+				String diseaseId = cols[1];
+				String pmid = cols[4];
+				try {
+					String key = String.format("%s-%s", geneId, diseaseId);
+					if (pmidMap.get(key) == null) {
+						pmidMap.put(key, new HashSet<Integer>());
+					}
+					pmidMap.get(key).add(Integer.parseInt(pmid));
+				} catch (NumberFormatException e) {
+					LOG.info(String.format("Unable to process the pmid: %s (%s, %s)", pmid, geneId, diseaseId));
+					continue;
+				}
+			}
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Cannot found pmidFile: " + fn);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void readDiseaseMap() {
+		String fn = diseaseMapFile.getName();
+		LOG.info(String.format("Parsing the file %s ......", fn));
+		System.out.println(String.format("Parsing the file %s ......", fn));
+		
+		try {
+			FileReader reader = new FileReader(diseaseMapFile);
+			Iterator<String[]> iterator = FormattedTextParser.parseTabDelimitedReader(reader);
+			
+			// first line is the header
+			iterator.next();
+			
+			while (iterator.hasNext()) {
+				String[] cols = iterator.next();
+				String diseaseId = cols[0];
+				String ontology = cols[2];
+				String code = cols[3];
+				
+				String ontologyId = "";
+				if (ontology.equals("DO")) {
+					ontologyId = "DOID:" + code;
+				} else if (ontology.equals("MSH")) {
+					ontologyId = code;
+				} else {
+					continue;
+				}
+				
+				if (ontologyIdMap.get(diseaseId) == null) {
+					ontologyIdMap.put(diseaseId, new HashSet<String>());
+				}
+				ontologyIdMap.get(diseaseId).add(ontologyId);
+			}
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Cannot found pmidFile: " + fn);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
 	private Map<String, String> geneMap = new HashMap<String, String>();
 	private String getGene(String geneId) throws ObjectStoreException {
 		String ret = geneMap.get(geneId);
@@ -94,11 +208,62 @@ public class DisgenetConverter extends BioFileConverter
 			Item item = createItem("DiseaseTerm");
 			item.setAttribute("identifier", identifier);
 			item.setAttribute("title", title);
-			ret = item.getIdentifier();
+			
+			if (ontologyIdMap.get(identifier) != null) {
+				for (String ontologyId : ontologyIdMap.get(identifier)) {
+					if (ontologyId.startsWith("DOID:")) {
+						item.addToCollection("xrefs", getDOTerm(ontologyId));
+					} else {
+						item.addToCollection("xrefs", getMeshTerm(ontologyId));
+					}
+				}
+			}
+			
 			store(item);
+			
+			ret = item.getIdentifier();
 			diseaseTermMap.put(identifier, ret);
 		}
 		return ret;
 	}
+
+	private Map<String, String> publicationMap = new HashMap<String, String>();
+	private String getPublication(String pubmedId) throws ObjectStoreException {
+		String ret = publicationMap.get(pubmedId);
+		if (ret == null) {
+			Item item = createItem("Publication");
+			item.setAttribute("pubMedId", pubmedId);
+			store(item);
+			ret = item.getIdentifier();
+			publicationMap.put(pubmedId, ret);
+		}
+		return ret;
+	}
+
+	private Map<String, String> ontologyItemMap = new HashMap<String, String>();
+	private String getDOTerm(String identifier) throws ObjectStoreException {
+		String ret = ontologyItemMap.get(identifier);
+		if (ret == null) {
+			Item item = createItem("DOTerm");
+			item.setAttribute("identifier", identifier);
+			store(item);
+			ret = item.getIdentifier();
+			ontologyItemMap.put(identifier, ret);
+		}
+		return ret;
+	}
+	private String getMeshTerm(String meshId) throws ObjectStoreException {
+		String ret = ontologyItemMap.get(meshId);
+		if (ret == null) {
+			Item item = createItem("MeshTerm");
+			item.setAttribute("identifier", meshId);
+			store(item);
+			ret = item.getIdentifier();
+			ontologyItemMap.put(meshId, ret);
+		}
+		return ret;
+	}
+	
+	
 
 }
